9c4dc3cea6d6c4c1520d6942bf53ab2393206c95
Aleksas <balc.aleksas@gmail.com>
2024-02-11 11:46:20 +0200

    Attempt adding server launch on github actions

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,11 +1,12 @@
 // @ts-check
 import { test, expect } from "@playwright/test"
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has heading', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  const heading = page.locator("h1");
+  await expect(heading).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
